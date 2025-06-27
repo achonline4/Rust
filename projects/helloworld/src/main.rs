e395d7df5dfbaaef5ed48173ebfe3a71d6e107d0
achonline4 <achonline4@gmail.com>
2025-06-27 05:47:35 +0530

    Update README

--- projects/helloworld/src/main.rs
+++ projects/helloworld/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+    println!("Guess the number");
+    let mut guess = String::new();
+
+    /* Write a separate function for this */
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("Your guess: {guess}");
 }
